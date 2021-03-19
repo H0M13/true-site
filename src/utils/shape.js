dffab1fbdfd849178e58ea9fb3cdbfda3caca3c3
samalot <samalotmedia@gmail.com>
2021-03-19 18:21:30 +0000

    initial init

--- src/utils/shape.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-
-
-export const labelsShape = PropTypes.shape({
-	projectTitle: PropTypes.string.isRequired
-});
--- src/views/Error/ErrorTypes.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const types = {
-
-}
-
-export const defaultProps = {
-
-}
